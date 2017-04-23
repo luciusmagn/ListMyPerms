883863c04092f15a94ba88beacbab3473aeaf7c3
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-23 15:17:24 +0200

    unretardize plugin

--- main.cs
+++ main.cs
@@ -9,46 +9,26 @@ using TShockAPI;
 
 namespace ListMyPerms
 {
-    [ApiVersion(1, 19)]
+    [ApiVersion(2, 1)]
     public class ListMyPerms : TerrariaPlugin
     {
-        
-    
-        public override Version Version
-        {
-            get { return new Version(1,0); }
-        }
-
-        public override string Name
-        {
-            get { return "ListMyPerms"; }
-        }
-
-        public override string Author
-        {
-            get { return "magnusi"; }
-        }
-
-        public override string Description
-        {
-            get { return "Lists permissions of all commands."; }
-        }
-
-        public ListMyPerms(Main game)
-            : base(game)
-        {
-        }
+        public override Version Version { get { return new Version(1,0); } }
+        public override string Name { get { return "ListMyPerms"; } }
+        public override string Author { get { return "magnusi"; } }
+        public override string Description { get { return "Lists permissions of all commands."; } }
+        public ListMyPerms(Main game) : base(game) { }
 
         public override void Initialize()
         {
             Commands.ChatCommands.Add(new Command("magnusi.perms", ListPerms, "listperms"));
             Commands.ChatCommands.Add(new Command("magnusi.findcmd", CmdFinder, "cmdfinder"));
         }
+
         public void ListPerms(CommandArgs args)
         {
             var i = 0;
             var index = 0;
-            
+
             Console.WriteLine("****LIST OF PERMISSIONS FOUND****");
             Console.WriteLine(Commands.ChatCommands.Count.ToString() + " commands found. ");
             try
@@ -56,19 +36,14 @@ namespace ListMyPerms
                 i = 0 + ((Convert.ToInt16(args.Parameters[0].ToString()) - 1) * 50);
                 Console.WriteLine("****Page " + args.Parameters[0] + " out of " + (Commands.ChatCommands.Count / 50).ToString() + "****");
             }
-            catch
-            {
-                Console.WriteLine("****Page 1 out of " + (Commands.ChatCommands.Count / 50).ToString() + "****");
-            }
-            
+            catch { Console.WriteLine("****Page 1 out of " + (Commands.ChatCommands.Count / 50).ToString() + "****"); }
 
             foreach (var command in Commands.ChatCommands)
             {
                 index++;
                 if (index > i && index < i + 50)
-                {
-                    switch (command.Permissions.Count) 
-                    { 
+                    switch (command.Permissions.Count)
+                    {
                         default:
                             string str = String.Join(", ", command.Permissions);
                             Console.WriteLine(str + ":" + command.Name);
@@ -77,7 +52,6 @@ namespace ListMyPerms
                             Console.WriteLine("no permissions :" + command.Name);
                             break;
                     }
-                }
             }
         }
         public void CmdFinder(CommandArgs args)
@@ -85,10 +59,7 @@ namespace ListMyPerms
             try
             {
                 var i = false;
-                
                 foreach (var command in Commands.ChatCommands)
-                {
-                    
                     if (command.Name.IndexOf(args.Parameters[0]) != -1)
                     {
                         if (i)
@@ -96,26 +67,19 @@ namespace ListMyPerms
                             args.Player.SendInfoMessage("Following commands contain searched term:");
                             i = false;
                         }
-                        try 
-                        { 
+                        try
+                        {
                                 string str = String.Join(", ", command.Permissions);
                                 args.Player.SendInfoMessage(str + ":" + command.Name);
                         }
-                        catch
-                        {
-                            args.Player.SendInfoMessage("no permissions :" + command.Name);
-                            
-                        }
+                        catch { args.Player.SendInfoMessage("no permissions :" + command.Name); }
                     }
-                }
             }
-            catch(Exception potato) 
+            catch(Exception potato)
             {
                 args.Player.SendErrorMessage("Command not found. Check your spelling and try again.");
                 if (args.Parameters.Contains("debug"))
-                {
                     Console.WriteLine(potato);
-                }
             }
         }
     }
